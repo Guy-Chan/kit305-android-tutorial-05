6b5236d185c9932dc4756382e1003aa206dc7ff4
Guy Chen <ychen99@utas.edu.au>
2024-03-27 17:04:04 +1100

    tutor 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,7 @@
 plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    //ADD THIS LINE, NOT THE OTHER LINES
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
